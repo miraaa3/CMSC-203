fc1788ce11ce9fe2f501808e4d1fcfdd064c09e4
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2022-09-13 21:42:38 +0800

    Add files via upload
    This is our first activity in CMSC203 under the supervision of Ms. Regina Rose Castillo.

---
+++ CMSC203_ACTIVITY1_ADOVAS/Sentence Builder Challenge/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String Firstname = "Miracle";
+        String Lastname = "Adovas";
+        int age = 19;
+        float GPA = 1.66f;
+        char BloodType = 'B';
+
+
+        System.out.println("Hi, My name is " + Firstname + " " + Lastname + ".");
+        System.out.println("I am " + age + " " + "years old" + ".");
+        System.out.println("My GPA is " + GPA + ".");
+        System.out.println("My bloodtype is " + BloodType + ". jn   ");
+
+
+
+
+        
+
+    }
+}
