e1eb6b9b72814d68fb4707097d9dc2b22d4b0160
Miraaawr <112455464+miraaa3@users.noreply.github.com>
2023-01-04 21:06:54 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY6_ADOVAS/AdovasMethods/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdovasMethods
+{
+    internal class Car
+    {
+        public string brand, modelname, color, speciality,speed;
+        public int releasedate,  price;
+
+        public Car(string brand, string modelname, string color, string speciality, string speed, int releasedate, int price, int v)
+        {
+            this.brand = brand;
+            this.modelname = modelname;
+            this.color = color;
+            this.speciality = speciality;
+            this.speed = speed;
+            this.releasedate = releasedate;
+            this.price = price;
+        }
+        public void launch()
+        {
+            Console.WriteLine("This will be the newly launch car of" + brand + "," + " the " + modelname + ".");
+        }
+
+        public void specifications()
+        {
+            Console.WriteLine("The Attributes are: " + color + " for the car color, and its average speed is " + speed + " with " + speciality + ".");
+        }
+
+    }
+}
---
+++ CMSC203_ACTIVITY6_ADOVAS/AdovasMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdovasMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car("Honda", "2023 Civic Si", "Glossy Black", "180-hp Turbocharged Engine", "215 km/h", 2023, 24, 650);
+            c.launch();
+            c.specifications();
+
+        }
+    }
+}
